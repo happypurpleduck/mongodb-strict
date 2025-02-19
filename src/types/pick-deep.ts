5ff4d3d8e4085f3d982be378ee66dbcab0e48c4a
happyduck <huasin.a.alsaeed@gmail.com>
2025-02-19 16:22:02 +0000

    :fire: pick-deep

--- src/types/pick-deep.ts
+++ src/types/pick-deep.ts
@@ -12,77 +12,6 @@ import type {
 	ObjectValue,
 } from "type-fest/source/internal";
 
-/**
-Pick properties from a deeply-nested object.
-
-It supports recursing into arrays.
-
-Use-case: Distill complex objects down to the components you need to target.
-
-@example
-```
-import type {PickDeep, PartialDeep} from 'type-fest';
-
-type Configuration = {
-	userConfig: {
-		name: string;
-		age: number;
-		address: [
-			{
-				city1: string;
-				street1: string;
-			},
-			{
-				city2: string;
-				street2: string;
-			}
-		]
-	};
-	otherConfig: any;
-};
-
-type NameConfig = PickDeep<Configuration, 'userConfig.name'>;
-// type NameConfig = {
-// 	userConfig: {
-// 		name: string;
-// 	}
-// };
-
-// Supports optional properties
-type User = PickDeep<PartialDeep<Configuration>, 'userConfig.name' | 'userConfig.age'>;
-// type User = {
-// 	userConfig?: {
-// 		name?: string;
-// 		age?: number;
-// 	};
-// };
-
-// Supports array
-type AddressConfig = PickDeep<Configuration, 'userConfig.address.0'>;
-// type AddressConfig = {
-// 	userConfig: {
-// 		address: [{
-// 			city1: string;
-// 			street1: string;
-// 		}];
-// 	};
-// }
-
-// Supports recurse into array
-type Street = PickDeep<Configuration, 'userConfig.address.1.street2'>;
-// type Street = {
-// 	userConfig: {
-// 		address: [
-// 			unknown,
-// 			{street2: string}
-// 		];
-// 	};
-// }
-```
-
-@category Object
-@category Array
-*/
 export type PickDeep<T, PathUnion extends Paths<T>> = T extends NonRecursiveType
 	? never
 	: T extends UnknownArray
@@ -101,9 +30,6 @@ export type PickDeep<T, PathUnion extends Paths<T>> = T extends NonRecursiveType
 				>
 			: never;
 
-/**
-Pick an object/array from the given object/array by one path.
-*/
 type InternalPickDeep<
 	T,
 	Path extends string | number,
@@ -115,9 +41,6 @@ type InternalPickDeep<
 			? Simplify<PickDeepObject<T, Path>>
 			: never;
 
-/**
-Pick an object from the given object by one path.
-*/
 type PickDeepObject<
 	RecordType extends object,
 	P extends string | number,
@@ -137,16 +60,11 @@ type PickDeepObject<
 			: never
 		: never;
 
-/**
-Pick an array from the given array by one path.
-*/
 type PickDeepArray<
 	ArrayType extends UnknownArray,
 	P extends string | number,
-> = P extends `${infer ArrayIndex extends number // Handle paths that are `${number}.${string}`
-}.${infer SubPath}`
-	? // When `ArrayIndex` is equal to `number`
-		number extends ArrayIndex
+> = P extends `${infer ArrayIndex extends number}.${infer SubPath}`
+	? number extends ArrayIndex
 		? ArrayType extends unknown[]
 			? Array<InternalPickDeep<NonNullable<ArrayType[number]>, SubPath>>
 			: ArrayType extends readonly unknown[]
@@ -154,8 +72,7 @@ type PickDeepArray<
 						InternalPickDeep<NonNullable<ArrayType[number]>, SubPath>
 					>
 				: never
-		: // When `ArrayIndex` is a number literal
-			ArrayType extends unknown[]
+		: ArrayType extends unknown[]
 			? [
 					...BuildTuple<ArrayIndex>,
 					InternalPickDeep<NonNullable<ArrayType[ArrayIndex]>, SubPath>,
@@ -166,13 +83,10 @@ type PickDeepArray<
 						InternalPickDeep<NonNullable<ArrayType[ArrayIndex]>, SubPath>,
 					]
 				: never
-	: // When the path is equal to `number`
-		P extends `${infer ArrayIndex extends number}`
-		? // When `ArrayIndex` is `number`
-			number extends ArrayIndex
+	: P extends `${infer ArrayIndex extends number}`
+		? number extends ArrayIndex
 			? ArrayType
-			: // When `ArrayIndex` is a number literal
-				ArrayType extends unknown[]
+			: ArrayType extends unknown[]
 				? [...BuildTuple<ArrayIndex>, ArrayType[ArrayIndex]]
 				: ArrayType extends readonly unknown[]
 					? readonly [...BuildTuple<ArrayIndex>, ArrayType[ArrayIndex]]
