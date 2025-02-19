9637c1bacb3eddd64c7a0cf80eaf36be869d7caf
happyduck <huasin.a.alsaeed@gmail.com>
2025-02-19 16:40:45 +0000

    augment 'omit-deep' for mongodb

--- src/types/omit-deep.ts
+++ src/types/omit-deep.ts
@@ -1,25 +1,18 @@
+import type { SimplifyDeep, UnionToTuple, UnknownArray } from "type-fest";
 import type {
-	LiteralUnion,
-	Paths,
-	SimplifyDeep,
-	UnionToTuple,
-	UnknownArray,
-	IsEqual,
-	IsNever,
-	ArraySplice,
-} from "type-fest";
-import type {
-	NonRecursiveType,
-	SetArrayAccess,
 	IsArrayReadonly,
-	ToString,
-	ExactKey,
+	SetArrayAccess,
 } from "type-fest/source/internal";
+import type { ExtendedPrimitive } from "./primitives";
+import type {
+	OmitDeepArrayWithOnePath,
+	OmitDeepObjectWithOnePath,
+} from "type-fest/source/omit-deep";
 
-export type OmitDeep<
-	T,
-	PathUnion extends LiteralUnion<Paths<T>, string>,
-> = SimplifyDeep<OmitDeepHelper<T, UnionToTuple<PathUnion>>, UnknownArray>;
+export type OmitDeep<T, PathUnion extends string> = SimplifyDeep<
+	OmitDeepHelper<T, UnionToTuple<PathUnion>>,
+	UnknownArray
+>;
 
 type OmitDeepHelper<T, PathTuple extends UnknownArray> = PathTuple extends [
 	infer Path,
@@ -31,52 +24,10 @@ type OmitDeepHelper<T, PathTuple extends UnknownArray> = PathTuple extends [
 type OmitDeepWithOnePath<
 	T,
 	Path extends string | number,
-> = T extends NonRecursiveType
+> = T extends ExtendedPrimitive
 	? T
 	: T extends UnknownArray
 		? SetArrayAccess<OmitDeepArrayWithOnePath<T, Path>, IsArrayReadonly<T>>
 		: T extends object
 			? OmitDeepObjectWithOnePath<T, Path>
 			: T;
-
-type OmitDeepObjectWithOnePath<
-	ObjectT extends object,
-	P extends string | number,
-> = P extends `${infer RecordKeyInPath}.${infer SubPath}`
-	? {
-			[Key in keyof ObjectT]: IsEqual<
-				RecordKeyInPath,
-				ToString<Key>
-			> extends true
-				? ExactKey<ObjectT, Key> extends infer RealKey
-					? RealKey extends keyof ObjectT
-						? OmitDeepWithOnePath<ObjectT[RealKey], SubPath>
-						: ObjectT[Key]
-					: ObjectT[Key]
-				: ObjectT[Key];
-		}
-	: ExactKey<ObjectT, P> extends infer Key
-		? IsNever<Key> extends true
-			? ObjectT
-			: Key extends PropertyKey
-				? Omit<ObjectT, Key>
-				: ObjectT
-		: ObjectT;
-
-type OmitDeepArrayWithOnePath<
-	ArrayType extends UnknownArray,
-	P extends string | number,
-> = P extends `${infer ArrayIndex extends number}.${infer SubPath}`
-	? number extends ArrayIndex
-		? Array<OmitDeepWithOnePath<NonNullable<ArrayType[number]>, SubPath>>
-		: ArraySplice<
-				ArrayType,
-				ArrayIndex,
-				1,
-				[OmitDeepWithOnePath<NonNullable<ArrayType[ArrayIndex]>, SubPath>]
-			>
-	: P extends `${infer ArrayIndex extends number}`
-		? number extends ArrayIndex
-			? []
-			: ArraySplice<ArrayType, ArrayIndex, 1, [unknown]>
-		: ArrayType;
