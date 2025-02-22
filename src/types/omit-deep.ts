e54b1d80049c53551d5a09dd86bf54f284dafe6f
happyduck <huasin.a.alsaeed@gmail.com>
2025-02-22 15:22:25 +0000

    fix(omit-deep): array element with ExtendedPrimitive

--- src/types/omit-deep.ts
+++ src/types/omit-deep.ts
@@ -1,13 +1,18 @@
-import type { SimplifyDeep, UnionToTuple, UnknownArray } from "type-fest";
 import type {
+	ArraySplice,
+	IsEqual,
+	IsNever,
+	SimplifyDeep,
+	UnionToTuple,
+	UnknownArray,
+} from "type-fest";
+import type {
+	ExactKey,
 	IsArrayReadonly,
 	SetArrayAccess,
+	ToString,
 } from "type-fest/source/internal";
 import type { ExtendedPrimitive } from "./primitives";
-import type {
-	OmitDeepArrayWithOnePath,
-	OmitDeepObjectWithOnePath,
-} from "type-fest/source/omit-deep";
 
 export type OmitDeep<T, PathUnion extends string> = SimplifyDeep<
 	OmitDeepHelper<T, UnionToTuple<PathUnion>>,
@@ -31,3 +36,47 @@ type OmitDeepWithOnePath<
 		: T extends object
 			? OmitDeepObjectWithOnePath<T, Path>
 			: T;
+
+type OmitDeepArrayWithOnePath<
+	ArrayType extends UnknownArray,
+	P extends string | number,
+> = P extends `${infer ArrayIndex extends number}.${infer SubPath}`
+	? number extends ArrayIndex
+		? Array<OmitDeepWithOnePath<NonNullable<ArrayType[number]>, SubPath>>
+		: ArraySplice<
+				ArrayType,
+				ArrayIndex,
+				1,
+				[OmitDeepWithOnePath<NonNullable<ArrayType[ArrayIndex]>, SubPath>]
+			>
+	: P extends `${infer ArrayIndex extends number}`
+		? number extends ArrayIndex
+			? []
+			: ArraySplice<ArrayType, ArrayIndex, 1, [unknown]>
+		: P extends string
+			? Array<OmitDeepWithOnePath<NonNullable<ArrayType[number]>, P>>
+			: ArrayType;
+
+type OmitDeepObjectWithOnePath<
+	ObjectT extends object,
+	P extends string | number,
+> = P extends `${infer RecordKeyInPath}.${infer SubPath}`
+	? {
+			[Key in keyof ObjectT]: IsEqual<
+				RecordKeyInPath,
+				ToString<Key>
+			> extends true
+				? ExactKey<ObjectT, Key> extends infer RealKey
+					? RealKey extends keyof ObjectT
+						? OmitDeepWithOnePath<ObjectT[RealKey], SubPath>
+						: ObjectT[Key]
+					: ObjectT[Key]
+				: ObjectT[Key];
+		}
+	: ExactKey<ObjectT, P> extends infer Key
+		? IsNever<Key> extends true
+			? ObjectT
+			: Key extends PropertyKey
+				? Omit<ObjectT, Key>
+				: ObjectT
+		: ObjectT;
