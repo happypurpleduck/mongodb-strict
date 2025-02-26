416edc9e08cdf4f7112baab51a11ed74e6ed9059
happyduck <huasin.a.alsaeed@gmail.com>
2025-02-26 05:37:20 +0000

    type-fest: rescope pick-deep internals

--- src/types/pick-deep.ts
+++ src/types/pick-deep.ts
@@ -1,6 +1,15 @@
-import type { Simplify, UnionToIntersection, UnknownArray } from "type-fest";
+import type {
+	IsNever,
+	Simplify,
+	UnionToIntersection,
+	UnknownArray,
+} from "type-fest";
+import type {
+	BuildObject,
+	BuildTuple,
+	ObjectValue,
+} from "type-fest/source/internal";
 import type { ExtendedPrimitive } from "./primitives";
-import type { PickDeepArray, PickDeepObject } from "type-fest/source/pick-deep";
 
 export type PickDeep<T, PathUnion extends string> = T extends ExtendedPrimitive
 	? never
@@ -30,3 +39,55 @@ type InternalPickDeep<
 		: T extends object
 			? Simplify<PickDeepObject<T, Path>>
 			: never;
+
+type PickDeepObject<
+	RecordType extends object,
+	P extends string | number,
+> = P extends `${infer RecordKeyInPath}.${infer SubPath}`
+	? ObjectValue<RecordType, RecordKeyInPath> extends infer ObjectV
+		? IsNever<ObjectV> extends false
+			? BuildObject<
+					RecordKeyInPath,
+					InternalPickDeep<NonNullable<ObjectV>, SubPath>,
+					RecordType
+				>
+			: never
+		: never
+	: ObjectValue<RecordType, P> extends infer ObjectV
+		? IsNever<ObjectV> extends false
+			? BuildObject<P, ObjectV, RecordType>
+			: never
+		: never;
+
+type PickDeepArray<
+	ArrayType extends UnknownArray,
+	P extends string | number,
+> = P extends `${infer ArrayIndex extends number}.${infer SubPath}`
+	? number extends ArrayIndex
+		? ArrayType extends unknown[]
+			? Array<InternalPickDeep<NonNullable<ArrayType[number]>, SubPath>>
+			: ArrayType extends readonly unknown[]
+				? ReadonlyArray<
+						InternalPickDeep<NonNullable<ArrayType[number]>, SubPath>
+					>
+				: never
+		: ArrayType extends unknown[]
+			? [
+					...BuildTuple<ArrayIndex>,
+					InternalPickDeep<NonNullable<ArrayType[ArrayIndex]>, SubPath>,
+				]
+			: ArrayType extends readonly unknown[]
+				? readonly [
+						...BuildTuple<ArrayIndex>,
+						InternalPickDeep<NonNullable<ArrayType[ArrayIndex]>, SubPath>,
+					]
+				: never
+	: P extends `${infer ArrayIndex extends number}`
+		? number extends ArrayIndex
+			? ArrayType
+			: ArrayType extends unknown[]
+				? [...BuildTuple<ArrayIndex>, ArrayType[ArrayIndex]]
+				: ArrayType extends readonly unknown[]
+					? readonly [...BuildTuple<ArrayIndex>, ArrayType[ArrayIndex]]
+					: never
+		: never;
