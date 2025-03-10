2f70094fd10ec0e87a78afd3bee73c20eb8f21e5
happyduck <huasin.a.alsaeed@gmail.com>
2025-03-10 06:21:56 +0000

    feat: pathOfType

--- src/types/path-of-type.ts
+++ src/types/path-of-type.ts
@@ -1,6 +1,14 @@
 import type { Get } from "./get.ts";
 import type { Paths } from "./path.ts";
 
+export type PathOfType<T, Type> = {
+	[K in keyof T]: K extends string
+		? Get<T, K> extends Type
+			? K
+			: never
+		: never;
+}[keyof T];
+
 export type PathsOfType<T, Type> = {
 	[K in Paths<T>]: K extends string
 		? Get<T, K> extends Type
