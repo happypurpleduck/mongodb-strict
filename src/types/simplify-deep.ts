295019c27dfb5d5172c8d2d78e9d7a460f94508a
happyduck <huasin.a.alsaeed@gmail.com>
2025-03-11 07:58:46 +0000

    feat(simplify-deep): add Exclude Type

--- src/types/simplify-deep.ts
+++ src/types/simplify-deep.ts
@@ -1,10 +1,12 @@
 import type { ExtendedPrimitive } from "./primitives.ts";
 
 // TODO: check effect on tuples
-export type SimplifyDeep<Type> = Type extends ExtendedPrimitive
+export type SimplifyDeep<Type, ExcludeType = never> = Type extends
+	| ExtendedPrimitive
+	| ExcludeType
 	? Type
 	: Type extends object[]
-		? Array<SimplifyDeep<Type[number]>>
+		? Array<SimplifyDeep<Type[number], ExcludeType>>
 		: Type extends object
-			? { [TypeKey in keyof Type]: SimplifyDeep<Type[TypeKey]> }
+			? { [TypeKey in keyof Type]: SimplifyDeep<Type[TypeKey], ExcludeType> }
 			: Type;
